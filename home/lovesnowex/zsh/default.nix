3a278aae6aebe5b90a3f72ae32c534ef50a2ad4e
lovesnowex <lsktw22@gmail.com>
2024-08-26 20:17:04 +0800

    Decouple zimfw configuration from zsh

--- home/lovesnowex/zsh/default.nix
+++ home/lovesnowex/zsh/default.nix
@@ -16,33 +16,6 @@
       fi
     '';
     autocd = true;
-    zimfw = {
-      enable = true;
-      degit = true;
-      disableVersionCheck = true;
-      zmodules = [
-        "environment"
-        "input"
-        "run-help"
-        "completion"
-        "zsh-users/zsh-completions --fpath src"
-        "zsh-users/zsh-autosuggestions"
-        "zsh-users/zsh-history-substring-search"
-        "zsh-users/zsh-syntax-highlighting"
-        "homebrew"
-        "archive"
-        "exa"
-        "ssh"
-        "termtitle"
-        "utility"
-        "Aloxaf/fzf-tab"
-        "wookayin/fzf-fasd"
-        "wfxr/forgit"
-        "MichaelAquilina/zsh-you-should-use"
-        "greymd/docker-zsh-completion"
-        "zdharma-continuum/fast-syntax-highlighting"
-        "joke/zim-github-cli"
-      ];
-    };
+    zimfw = import ./zimfw.nix;
   };
 }
