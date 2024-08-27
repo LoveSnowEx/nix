072e84e3e51e8f32798ccc7aae017d71dc41fa60
LoveSnowEx <lsktw22@gmail.com>
2024-08-27 12:29:57 +0800

    Update git configuration with signing

--- home/lovesnowex/git/default.nix
+++ home/lovesnowex/git/default.nix
@@ -1,18 +1,23 @@
 { ... }:
 {
   programs.git = {
-    # Sign
-    # signing = {
-    #   signByDefault = true;
-    #   key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlmEeuqIj2EQuHMhiua+C34/mibY9KeMwAyQNj299nl";
-    # };
-    # extraConfig = {
-    #   gpg = {
-    #     format = "ssh";
-    #     ssh = {
-    #       program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-    #     };
-    #   };
-    # };
+    enable = true;
+    userName = "LoveSnowEx";
+    userEmail = "lsktw22@gmail.com";
+    signing = {
+      signByDefault = true;
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICumxsUjMJPkNIyLlj4L1BYRYs6gBoozSDQw1GEVKWt5 lsktw22@gmail.com";
+    };
+    extraConfig = {
+      commit.gpgSign = true;
+      gpg = {
+        format = "ssh";
+        ssh = {
+          defaultKeyCommand = "ssh-add -L";
+          allowedSignersFile = "~/.ssh/allowed_signers";
+        };
+      };
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
   };
 }
