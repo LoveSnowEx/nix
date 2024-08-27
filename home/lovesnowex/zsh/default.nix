2c6a72ab1b5efc396900a2800271730e5847a7a9
LoveSnowEx <lsktw22@gmail.com>
2024-08-27 14:37:47 +0800

    Update zsh configuration with additional programs

--- home/lovesnowex/zsh/default.nix
+++ home/lovesnowex/zsh/default.nix
@@ -3,19 +3,41 @@
   imports = [
     ./starship.nix
   ];
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = false;
+      completionInit = ''
+        autoload -Uz compinit
+        if [[ ! -e ~/.zcompdump(N.mh+24) ]]; then
+          compinit
+        else
+          compinit -C
+        fi
+      '';
+      autocd = true;
+      zprof.enable = true;
+      zimfw = import ./zimfw.nix;
+    };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = false;
-    completionInit = ''
-      autoload -Uz compinit
-      if [[ ! -e ~/.zcompdump(N.mh+24) ]]; then
-        compinit
-      else
-        compinit -C
-      fi
-    '';
-    autocd = true;
-    zimfw = import ./zimfw.nix;
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
 }
