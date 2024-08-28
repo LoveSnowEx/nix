f9e0b7657125ba2627f7808a70e71a080c016a56
LoveSnowEx <lsktw22@gmail.com>
2024-08-28 17:48:11 +0800

    Optimize zsh for completion history and highlighting

--- home/lovesnowex/shell/zsh.nix
+++ home/lovesnowex/shell/zsh.nix
@@ -2,7 +2,7 @@
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = false;
+    enableCompletion = true;
     completionInit = ''
       autoload -Uz compinit
       if [[ ! -e ~/.zcompdump(N.mh+24) ]]; then
@@ -13,5 +13,16 @@
     '';
     autocd = true;
     zimfw = import ./zimfw.nix;
+    history = {
+      expireDuplicatesFirst = true;
+      extended = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+    };
+    initExtra = ''
+      setopt HIST_FIND_NO_DUPS
+      export HISTORY_SUBSTRING_SEARCH_PREFIXED=1
+      export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+    '';
   };
 }
