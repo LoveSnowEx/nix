85a630244aa921b7e36610045e19d5c25755d88f
LoveSnowEx <lsktw22@gmail.com>
2024-09-30 20:30:10 +0800

    Add go bin directory to PATH in zsh.nix

--- home/lovesnowex/shell/zsh.nix
+++ home/lovesnowex/shell/zsh.nix
@@ -23,6 +23,7 @@
       setopt HIST_FIND_NO_DUPS
       export HISTORY_SUBSTRING_SEARCH_PREFIXED=1
       export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+      export PATH=$HOME/go/bin:$PATH
     '';
     shellAliases = {
       b = "bat";
