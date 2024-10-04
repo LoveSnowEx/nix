f997e0e45acc699fc3fff4c58048eb7300011c29
LoveSnowEx <lsktw22@gmail.com>
2024-10-04 22:42:08 +0800

    Remove substituters from ustc.edu.cn

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "A very basic flake";
 
-  # the nixConfig here only affects the flake itself, not the system configuration!
-  nixConfig = {
-    substituters = [
-      # Query the mirror of USTC first, and then the official cache.
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
-    ];
-  };
-
   inputs = {
     # NixPKG
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
